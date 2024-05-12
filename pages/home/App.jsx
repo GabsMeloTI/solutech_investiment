601b525e480299c7b5ef8c5da3f4ae88c78aa006
gabsmelin <rm550202@fiap.com.br>
2024-05-12 10:23:59 -0300

    Página home finalizada.

--- pages/home/App.jsx
+++ pages/home/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { StatusBar, Image, StyleSheet, Text, View } from 'react-native';
-const logo = require('./assets/logo.png');
-const imgNoticia = require('./assets/exp-noticia.png');
+const logo = require('../../assets/logo.png');
+const imgNoticia = require('../../assets/exp-noticia.png');
+const instagram = require('../../assets/instagram.png');
+const facebook = require('../../assets/facebook.png');
+const twitter = require('../../assets/twitterX.png');
+const rodape = require('../../assets/linha-rodape.png');
 
-export default function Home() {
+export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
 
@@ -16,7 +20,7 @@ export default function Home() {
           <Text style={styles.topicos}>Moedas</Text>
           <Text style={styles.topicos}>Cálculos</Text>
           <Text style={styles.topicos}>Sobre nós</Text>
-          <Text style={styles.topicos}>Entrar</Text>
+          <Text style={styles.topicos} onPress={() => {navigation.navigate('SingIn');}}>Entrar</Text>
         </View>
       </View>
 
@@ -53,6 +57,38 @@ export default function Home() {
             </View>
           </View>
         </View>
+
+        <View style={styles.rodape}>
+                <View>
+                    <Image source={rodape} style={styles.linhaRodape}/>
+                </View>
+                
+                <View style={styles.conteudoRodape}>
+                    <View>
+                        <Text style={styles.textRodape}>Investir no mercado financeiro apresenta desafios, desde entender os produtos até gerenciar riscos. É crucial compreender as dificuldades dos investidores para melhorar suas experiências e resultados.</Text>
+                    </View>
+                    <View>
+                        <View>
+                            <Text style={styles.textRedes}>Nossas Redes Sociais:</Text>
+                        </View>
+                        <View>
+                            <View style={styles.imageRedes}>
+                                <Image source={facebook} style={styles.imgRodape}/>
+                                <Text style={styles.textRedes}>Solutech Investments</Text>
+                            </View>
+                            <View style={styles.imageRedes}>
+                                <Image source={instagram} style={styles.imgRodape}/>
+                                <Text style={styles.textRedes}>Solutech</Text>
+                            </View>
+                            <View style={styles.imageRedes}>
+                                <Image source={twitter} style={styles.imgRodape}/>
+                                <Text style={styles.textRedes}>solutech_investments</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
+
       </View>
       <StatusBar style="auto" />
     </View>
@@ -63,7 +99,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    marginTop: '5%',
     backgroundColor: '#E8E9E4', 
   },
   menu: {
@@ -124,5 +159,38 @@ const styles = StyleSheet.create({
   textNoticia: {
     fontSize: 12,
     padding:  10,
-  }
+  },
+
+  rodape: {
+    flexDirection: "column",
+    width: '100%',
+  },
+  conteudoRodape: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: "row",
+    width: '100%',
+    paddingHorizontal: '7%'
+  },
+  textRodape: {
+    width: 180,
+    fontSize: 12,
+  },
+  textRedes: {
+    fontSize: 12,
+    marginBottom: '10%',
+  },
+  imageRedes: {
+    flexDirection: "row",
+  },
+  linhaRodape: {
+    height: 2,
+    marginTop: '10%',
+    width: '100%',
+    marginBottom: '5%',
+  },
+  imgRodape: {
+    width: 17,
+    height: 17,
+  },
 });
