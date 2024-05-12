a1d28ee3ed22c34071850602f3eefdac99812637
gabsmelin <rm550202@fiap.com.br>
2024-05-12 10:24:21 -0300

    Arrumando rotas de navegação.

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Home } from './pages/home/App';
-
+import Home from './pages/home/App.jsx';
+import SingIn from './pages/singIn/App.jsx';
+import SingUp from './pages/singUp/App.jsx';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,8 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SingIn" component={SingIn} />
+        <Stack.Screen name="SingUp" component={SingUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
