7e538773ded81826d7658fc1342509fca205bdd6
gabsmelin <rm550202@fiap.com.br>
2024-05-14 15:44:33 -0300

    Adicionando rota para a página de moeda.

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/home/App.jsx';
 import SingIn from './pages/singIn/App.jsx';
 import SingUp from './pages/singUp/App.jsx';
 import InvestmentForm from './pages/investmentForm/App.jsx';
+import Moeda from './pages/moeda/App.jsx';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ export default function AppNavigator() {
         <Stack.Screen name="SingIn" component={SingIn} />
         <Stack.Screen name="SingUp" component={SingUp} />
         <Stack.Screen name="InvestmentForm" component={InvestmentForm} />
+        <Stack.Screen name="Moeda" component={Moeda} />
       </Stack.Navigator>
     </NavigationContainer>
   );
