336834f6f0bd493a6a1365dd00fdff0a8b27066c
gabsmelin <rm550202@fiap.com.br>
2024-05-14 19:41:14 -0300

    Adicionando função para realização de login.

--- pages/singIn/App.jsx
+++ pages/singIn/App.jsx
@@ -1,117 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { StatusBar, Image, StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StatusBar, Image, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+
 const logo = require('../../assets/logo.png');
 const rodape = require('../../assets/linha-rodape.png');
 const instagram = require('../../assets/instagram.png');
 const facebook = require('../../assets/facebook.png');
 const twitter = require('../../assets/twitterX.png');
 
-
 const api = axios.create({
-  baseURL : "https://solutech-fiap-default-rtdb.firebaseio.com/"
-})
-
+  baseURL: "https://solutech-fiap-default-rtdb.firebaseio.com/"
+});
 
 export default function SingIn({ navigation }) {
-  const [usuario, setUsuario] = useState({
-    nome: "",
-    email: "",
-    senha: "",
-  });
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-  function logar() {
-    api.get('/usuarios.json').then((dataset))
-  }
+  const login = async () => {
+    try {
+      const response = await api.get('/usuarios.json');
+      const data = response.data;
+
+      let userFound = false;
+
+      for (const key in data) {
+        if (data[key].email === email && data[key].senha === senha) {
+          userFound = true;
+          alert("Login bem-sucedido!", `Bem-vindo, ${data[key].nome}!`);
+          navigation.navigate('Home');
+          break;
+        } else if (data[key].email != email && data[key].senha === senha) {
+          alert("Erro de login, o email está incorreto. Tente novamente.");
+        } else if (data[key].email === email && data[key].senha != senha) {
+          alert("Erro de login, a senha está incorreta. Tente novamente.");
+        }
+      }
+
+
+    } catch (error) {
+      alert("Erro", `Erro ao fazer login: ${error.message}`);
+    }
+  };
 
   return (
     <View style={styles.container}>
-
       <View style={styles.menu}>
         <View>
-          <Image source={logo} style={styles.logo}/>
+          <Image source={logo} style={styles.logo} />
         </View>
         <View style={styles.lista}>
-        <Text style={styles.topicos} onPress={() => {navigation.navigate('Home');}}>Notícias</Text>
+          <Text style={styles.topicos} onPress={() => { navigation.navigate('Home'); }}>Notícias</Text>
           <Text style={styles.topicos}>Moedas</Text>
           <Text style={styles.topicos}>Cálculos</Text>
           <Text style={styles.topicos}>Sobre nós</Text>
-          <Text style={styles.topicos} onPress={() => {navigation.navigate('SingIn');}}>Entrar</Text>
+          <Text style={styles.topicos} onPress={() => { navigation.navigate('SingIn'); }}>Entrar</Text>
         </View>
       </View>
 
       <View style={styles.conteudo}>
-        <View  style={styles.conteudoTitle}>
+        <View style={styles.conteudoTitle}>
           <Text style={styles.titulo}>Entrar</Text>
           <Text style={styles.subtitulo}>Ainda não tem conta em nossa plataforma?</Text>
-          <Text style={styles.subtitulo}>Não perca tempo e 
-            <Text style={styles.span} onPress={() => {navigation.navigate('SingUp');}}> cadastra-se </Text>
-          para ficar por dentro do mundo dos investimentos.</Text>
+          <Text style={styles.subtitulo}>Não perca tempo e
+            <Text style={styles.span} onPress={() => { navigation.navigate('SingUp'); }}> cadastra-se </Text>
+            para ficar por dentro do mundo dos investimentos.
+          </Text>
         </View>
 
         <View style={styles.conteudoInput}>
-            <View style={styles.conteudoTitle}>
-                <Text style={styles.titleInput}>BEM-VINDO À SOLUTECH FINANCE!</Text>
-                <Text style={styles.desInput}>Aqui, você encontrará tudo que precisa para começar sua jornada rumo ao sucesso financeiro. Explore nossos recursos e esteja pronto para investir no seu futuro. Estamos aqui para ajudá-lo a cada passo do caminho. Boa sorte e bons investimentos!</Text>
+          <View style={styles.conteudoTitle}>
+            <Text style={styles.titleInput}>BEM-VINDO À SOLUTECH FINANCE!</Text>
+            <Text style={styles.desInput}>Aqui, você encontrará tudo que precisa para começar sua jornada rumo ao sucesso financeiro. Explore nossos recursos e esteja pronto para investir no seu futuro. Estamos aqui para ajudá-lo a cada passo do caminho. Boa sorte e bons investimentos!</Text>
+          </View>
+          <View>
+            <View>
+              <Text style={styles.desInput}>Email:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Digite seu email"
+                value={email}
+                onChangeText={setEmail}
+              />
             </View>
             <View>
-                <View>
-                    <Text style={styles.desInput}>Email:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Digite seu email"
-                    />
-                </View>
-                <View>
-                    <Text style={styles.desInput}>Senha:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Digite sua senha"
-                        secureTextEntry={true}
-                    />
-                </View>
-                <TouchableOpacity style={styles.botao}>
-                    <Text style={styles.textoBotao}>Entrar</Text>
-                </TouchableOpacity>
-
+              <Text style={styles.desInput}>Senha:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Digite sua senha"
+                secureTextEntry={true}
+                value={senha}
+                onChangeText={setSenha}
+              />
             </View>
+            <TouchableOpacity style={styles.botao} onPress={login}>
+              <Text style={styles.textoBotao}>Entrar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
 
-            <View style={styles.rodape}>
-                <View>
-                    <Image source={rodape} style={styles.linhaRodape}/>
-                </View>
-                
-                <View style={styles.conteudoRodape}>
-                    <View>
-                        <Text style={styles.textRodape}>Investir no mercado financeiro apresenta desafios, desde entender os produtos até gerenciar riscos. É crucial compreender as dificuldades dos investidores para melhorar suas experiências e resultados.</Text>
-                    </View>
-                    <View>
-                        <View>
-                            <Text style={styles.textRedes}>Nossas Redes Sociais:</Text>
-                        </View>
-                        <View>
-                            <View style={styles.imageRedes}>
-                                <Image source={facebook} style={styles.imgRodape}/>
-                                <Text style={styles.textRedes}>Solutech Investments</Text>
-                            </View>
-                            <View style={styles.imageRedes}>
-                                <Image source={instagram} style={styles.imgRodape}/>
-                                <Text style={styles.textRedes}>Solutech</Text>
-                            </View>
-                            <View style={styles.imageRedes}>
-                                <Image source={twitter} style={styles.imgRodape}/>
-                                <Text style={styles.textRedes}>solutech_investments</Text>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            
+        <View style={styles.rodape}>
+          <View>
+            <Image source={rodape} style={styles.linhaRodape} />
+          </View>
 
+          <View style={styles.conteudoRodape}>
+            <View>
+              <Text style={styles.textRodape}>Investir no mercado financeiro apresenta desafios, desde entender os produtos até gerenciar riscos. É crucial compreender as dificuldades dos investidores para melhorar suas experiências e resultados.</Text>
+            </View>
+            <View>
+              <View>
+                <Text style={styles.textRedes}>Nossas Redes Sociais:</Text>
+              </View>
+              <View>
+                <View style={styles.imageRedes}>
+                  <Image source={facebook} style={styles.imgRodape} />
+                  <Text style={styles.textRedes}>Solutech Investments</Text>
+                </View>
+                <View style={styles.imageRedes}>
+                  <Image source={instagram} style={styles.imgRodape} />
+                  <Text style={styles.textRedes}>Solutech</Text>
+                </View>
+                <View style={styles.imageRedes}>
+                  <Image source={twitter} style={styles.imgRodape} />
+                  <Text style={styles.textRedes}>solutech_investments</Text>
+                </View>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
 
-    
       <StatusBar style="auto" />
     </View>
   );
@@ -121,7 +139,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#E8E9E4', 
+    backgroundColor: '#E8E9E4',
   },
   menu: {
     alignItems: 'center',
@@ -148,7 +166,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: '5%',
     width: 350,
-    
   },
   titulo: {
     fontSize: 26,
@@ -180,7 +197,6 @@ const styles = StyleSheet.create({
     color: '#E4A96A',
     fontWeight: 'bold',
     marginBottom: '2%',
-    
   },
   input: {
     height: 40,
@@ -194,17 +210,16 @@ const styles = StyleSheet.create({
   },
   botao: {
     marginTop: '5%',
-    backgroundColor: '#f2f2f2', 
-    paddingHorizontal: '12%', 
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: '12%',
     paddingVertical: '3%',
     borderRadius: 10,
   },
   textoBotao: {
-    color: '#666', 
-    fontSize: 12, 
-    textAlign: 'center', 
+    color: '#666',
+    fontSize: 12,
+    textAlign: 'center',
   },
-  
   rodape: {
     flexDirection: "column",
     width: '100%',
@@ -214,7 +229,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: "row",
     width: '100%',
-    paddingHorizontal: '7%'
+    paddingHorizontal: '7%',
   },
   textRodape: {
     width: 180,
