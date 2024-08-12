6c0972653ee546cb8b5fb0a7de22e2f72c4e7473
GabsMeloTI <rm550202@fiap.com.br>
2024-08-12 06:11:40 -0300

    style - add estilização a página.

--- pages/editUser/App.jsx
+++ pages/editUser/App.jsx
@@ -69,5 +69,69 @@ export default function EditUser({ navigation }) {
 
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#E8E9E4',
+  },
+  conteudo: {
+    alignItems: 'center',
+  },
+  conteudoTitle: {
+    alignItems: 'center',
+    marginBottom: '5%',
+    width: 350,
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#797777',
+  },
+  subtitulo: {
+    fontSize: 12,
+    color: '#797777',
+    textAlign: 'center',
+  },
+  span: {
+    color: '#E4A96A',
+  },
+  conteudoInput: {
+    paddingHorizontal: '3%',
+    paddingVertical: '12%',
+    alignItems: 'center',
+    backgroundColor: '#20515E',
+  },
+  desInput: {
+    fontSize: 10,
+    color: '#E4A96A',
+    width: '80%',
+    marginBottom: '2%',
+  },
+  titleInput: {
+    fontSize: 16,
+    color: '#E4A96A',
+    fontWeight: 'bold',
+    marginBottom: '2%',
+  },
+  input: {
+    height: 40,
+    width: 280,
+    borderColor: '#20515E',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    backgroundColor: '#f2f2f2',
+  },
+  botao: {
+    marginTop: '5%',
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: '12%',
+    paddingVertical: '3%',
+    borderRadius: 10,
+  },
+  textoBotao: {
+    color: '#666',
+    fontSize: 12,
+    textAlign: 'center',
+  },
 });
