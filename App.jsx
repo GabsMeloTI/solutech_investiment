99aba48e7a8fe485e16644f333c4637cfa1c72a8
GabsMeloTI <rm550202@fiap.com.br>
2024-08-12 06:15:52 -0300

    feat - add exportação da nova página.

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { UserProvider } from './components/dataUser/App.jsx';
 import Home from './pages/home/App.jsx';
 import SingIn from './pages/singIn/App.jsx';
 import SingUp from './pages/singUp/App.jsx';
@@ -9,7 +10,7 @@ import CurrencyExchange from './pages/currencyExchange/App.jsx';
 import Calculation from './pages/calculation/App.jsx';
 import AboutUs from './pages/aboutUs/App.jsx';
 import CalculationResult from './pages/calculationResult/App.jsx';
-import { UserProvider } from './components/dataUser/App.jsx';
+import EditUser from './pages/editUser/App.jsx';
 
 const Stack = createStackNavigator();
 
@@ -25,7 +26,8 @@ export default function AppNavigator() {
           <Stack.Screen name="CurrencyExchange" component={CurrencyExchange} />
           <Stack.Screen name="Calculation" component={Calculation} />
           <Stack.Screen name="AboutUs" component={AboutUs} />
-          <Stack.Screen name="calculationResult" component={CalculationResult} />
+          <Stack.Screen name="CalculationResult" component={CalculationResult} />
+          <Stack.Screen name="EditUser" component={EditUser} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserProvider>
